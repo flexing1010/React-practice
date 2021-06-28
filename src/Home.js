5bf2a5d52b4c0202050f57f75da9a8b64baf2f07
Jeongjin Kim <kjzon@hotmail.com>
2021-06-28 22:55:59 +0900

    Finished

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,18 @@
-import { useState } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  // let name = "mario";
-  const [name, setName] = useState("mario");
-  const [age, setAge] = useState(22);
-  const handleClick = () => {
-    setName("luigi");
-    setAge(32);
-  };
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      <button onClick={handleClick}>Click me</button>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="HELLO" />}
     </div>
   );
 };
